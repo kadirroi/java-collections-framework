1dad6f67b15bc21d23b97e6559a09a7db2270a94
Roi <Roi@Kadirroi>
2018-05-13 19:46:41 +0300

    format fix

--- JavaCollectionsFramework/src/com/roi/collections/example/arraylist/JavaArrayListRemoveIfWithJava8.java
+++ JavaCollectionsFramework/src/com/roi/collections/example/arraylist/JavaArrayListRemoveIfWithJava8.java
@@ -7,44 +7,43 @@ import java.util.function.Predicate;
 public class JavaArrayListRemoveIfWithJava8 {
 
 	public static void main(String[] args) {
-	
-		
-     //Defining a new list to append  new elements
-		List<String>  countries = new ArrayList<>();
+
+		// Defining a new list to append new elements
+		List<String> countries = new ArrayList<>();
 		countries.add("Turkey");
 		countries.add("USA");
 		countries.add("England");
 		countries.add("Spain");
-			
-		
-		CustomPredicate filter = new JavaArrayListRemoveIfWithJava8().new CustomPredicate();	
-		
-		//Predicate<? super E> filter
-		//The old school way
+
+		CustomPredicate filter = new JavaArrayListRemoveIfWithJava8().new CustomPredicate();
+
+		// Predicate<? super E> filter
+		// The old school way
 		countries.removeIf(filter);
-		
-		//Let's look at final status 
-		//[USA, England, Spain]
+
+		// Let's look at final status
+		// [USA, England, Spain]
 		System.out.println(countries);
-		
-		//RemoIf was added in Java 1.8 and it is useful 
-		//method to remove element with condition related filter 
-		countries.removeIf(country -> {return "USA".equals(country);});
-		
-		//Let's look at final status 
-		//[England, Spain]
+
+		// RemoIf was added in Java 1.8 and it is useful
+		// method to remove element with condition related filter
+		countries.removeIf(country -> {
+			return "USA".equals(country);
+		});
+
+		// Let's look at final status
+		// [England, Spain]
 		System.out.println(countries);
-		
 
 	}
-	
-	class CustomPredicate implements Predicate<String>{
+
+	class CustomPredicate implements Predicate<String> {
 
 		@Override
 		public boolean test(String t) {
 			return "Turkey".equals(t);
 		}
-		
+
 	}
 
 }
